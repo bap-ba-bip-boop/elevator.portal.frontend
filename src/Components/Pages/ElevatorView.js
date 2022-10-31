256e58f77b1ff629f43b71e8bb824c6b2c4ae2e0
Calle <calle.be@telia.com>
2022-10-31 16:17:40 +0100

    Ok first iteration

--- src/Components/Pages/ElevatorView.js
+++ src/Components/Pages/ElevatorView.js
@@ -5,59 +5,63 @@ import elevatorViewData from './ElevatorSettingsData.json'
 
 export const ElevatorView = (props) => {
 
-    //address, "GET", 
-    /*
-    Header:
-        "Headers" : {
-            "Content-Type": "application/json"
-        }
-    */
-
-    const [elevator, setElevator] = useState(elevatorViewData['ElevatorView-ViewModel']);
+    const [elevator, setElevator] = useState(null);
+    const [deviceMethodResponse, setDeviceMethodResponse] = useState(null)
 
     useEffect(()=>{
-        //var elevatordata = getData(
-        //    elevatorViewData.apiElevatorViewUrl,//kommer säkerligen behövas skrivas om
-        //    elevatorViewData.apiElevatorViewMethod,
-        //    elevatorViewData.apiElevatorViewHeaders
-        //);
-        var elevatordata = {
-            "ElevatorId" : "Elevator1",
-            "ElevatorName" : "Elevator1",
-            "BuildingName" : "Building1",
-            "isFunctioning": true,
-            "Status": "Going from some floor to another",
-            "AreDoorsOpen": false
-        };
-
-        //för test
-        console.log(elevatordata);
-
-        setElevator(elevatordata)
-
+        getData(
+            `${elevatorViewData.apiElevatorViewUrl}/${props.ElevatorId}`,//kommer säkerligen behövas skrivas om
+            elevatorViewData.apiElevatorViewMethod,
+            elevatorViewData.apiElevatorViewHeaders
+        ).then(
+            result => {
+                setElevator(result);
+            }
+        );
     },[])
 
-    const sendDeviceMethodCall = (methodName) =>
+    const sendDeviceMethodCall = (event) =>
     {
+        event.preventDefault();
         var messageBody = elevatorViewData.DeviceMethodCallBody;
 
-        messageBody.ElevatorId = elevator.ElevatorId;
-        messageBody.FunctionName = methodName;
+        messageBody.ElevatorId = elevator.elevatorId;
+        messageBody.FunctionName = "methodName";
 
-        console.log("event sent with method body: ", messageBody)
+        fetch(
+            elevatorViewData.apiDeviceMethodCallUrl,
+            {
+                method: elevatorViewData.apiDeviceMethodCallMethod,
+                headers: elevatorViewData.apiDeviceMethodHeaders,
+                body: JSON.stringify(messageBody)
+            }
+        )
+        .then(r => r.json())
+        .then(
+            result => {
+                var response = elevatorViewData.DeviceMethodCallResponse;
+                response.Message = result.message;
+                response.Success = result.success;
+                console.log(response);
+                setDeviceMethodResponse(response);
+            }
+        ).catch(err => {
+            console.log("Error Posting data: " + err);
+          });
     }
 
     return (
         <>
-            <h2>{elevator.ElevatorName}</h2>
-            <h3>{elevator.BuildingName}</h3>
-            <p>{ elevator.isFunctioning === true ? "Elevator is Functioning" : "Elevator does not Function"}</p>
-            <p>{elevator.Status}</p>
-            <p>{ elevator.AreDoorsOpen === true ? "Doors are open" : "Doors are not open"}</p>
+            <h2>{elevator && elevator.elevatorName}</h2>
+            <h3>{elevator && elevator.buildingName}</h3>
+            <p>{elevator && elevator.isFunctioning === true ? "Elevator is Functioning" : "Elevator does not Function"}</p>
+            <p>{elevator && elevator.status}</p>
+            <p>{elevator && elevator.areDoorsOpen === true ? "Doors are open" : "Doors are not open"}</p>
 
             <div>
                 <h2>Button Panel:</h2>
-                <button onClick={() => sendDeviceMethodCall("OpenClose")}>Open Doors</button>
+                <input type="submit"  onClick={e => sendDeviceMethodCall(e)} value="Open Doors"/>
+                {deviceMethodResponse && (deviceMethodResponse.Success === true ? <p>{deviceMethodResponse.Message}</p> : <p>{deviceMethodResponse.Message}</p>)}
             </div>
         </>
     )
