f4689a0b0f4d4be402dc09299dc6c10b875bfcab
David Aslan <davidaslan112671@live.se>
2022-11-02 16:34:40 +0100

    ..

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import TechnicianRequestBtn from "./Components/TechnicianRequestBtn";
 
 function App() {
   return (
@@ -9,14 +10,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
+        <TechnicianRequestBtn />
       </header>
     </div>
   );
