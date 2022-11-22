fe7c7ba1931643af1ebee02380628d3082556951
David Aslan <davidaslan112671@live.se>
2022-11-22 11:24:48 +0100

    Card color conditions added

--- src/Components/Reports/ReportCard.jsx
+++ src/Components/Reports/ReportCard.jsx
@@ -2,40 +2,39 @@ import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
-import {StyledLink} from "../Base/StyledLink.jsx";
+import { StyledLink } from "../Base/StyledLink.jsx";
 
-import { parseJSON, differenceInSeconds} from 'date-fns'
-import React from 'react'
+import { parseJSON, differenceInSeconds } from "date-fns";
+import React from "react";
 
-export const ReportCard = ({Report}) => {
-
+export const ReportCard = ({ Report }) => {
+  console.log("Report: ", Report);
   const calculateTimeLeft = (JSONTime) => {
     console.log(JSONTime);
     const diff = differenceInSeconds(parseJSON(JSONTime), new Date());
     const days = Math.floor(diff / 86400);
     const hours = Math.floor((diff - days * 86400) / 3600);
     const minutes = Math.floor((diff - days * 86400 - hours * 3600) / 60);
-  
+
     return `${days} Days, ${hours} Hours, and ${minutes} Minutes`;
+  };
+
+  function BgColorBasedOnDeadLineAndFinishDate(Report) {
+    if (Report.isDone) return "green";
+    if (Date.parse(Report.deadline) <= new Date()) return "red";
+    if (Date.parse(Report.deadline) > new Date() && !Report.isDone) return "yellow";
+    return "hotpink";
   }
 
   return (
-    <Card sx={{ minWidth: 275}}>
+    <Card sx={{ minWidth: 275, bgcolor: BgColorBasedOnDeadLineAndFinishDate(Report) }}>
       <CardContent>
+        <Typography>{Report.elevatorName}</Typography>
+        <Typography>Address: {Report.address}</Typography>
+        <Typography component="">Deadline: {Report.deadline.split("T")[0]}</Typography>
+        <Typography>Time Left: {calculateTimeLeft(Report.deadline)}</Typography>
         <Typography>
-          {Report.elevatorName}
-        </Typography>
-        <Typography>
-          Address: {Report.address}
-        </Typography>
-        <Typography component="">
-          Deadline: {Report.deadline.split("T")[0]}
-        </Typography>
-        <Typography>
-          Time Left: {calculateTimeLeft(Report.deadline)}
-        </Typography>
-        <Typography>
-          Assigned Technician: { Report.assignedTechnician != null ? Report.technicianName : "None Assigned"}
+          Assigned Technician: {Report.assignedTechnician != null ? Report.technicianName : "None Assigned"}
         </Typography>
         <Typography>
           Tasks Done: {Report.tasksDone}/{Report.totalTasks}
@@ -45,5 +44,5 @@ export const ReportCard = ({Report}) => {
         <StyledLink to={Report.id}>Show Report</StyledLink>
       </CardActions>
     </Card>
-  )
-}
+  );
+};
