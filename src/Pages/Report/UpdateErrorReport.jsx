a7b3ce96b95c77e1ad334f73327e395905c1b722
Melvin Anderhagen <anderhagen.melvin@gmail.com>
2022-12-02 15:29:26 +0100

    Deleted selectedelevatorview and added styling up

--- src/Pages/Report/UpdateErrorReport.jsx
+++ src/Pages/Report/UpdateErrorReport.jsx
@@ -5,11 +5,10 @@ import { GetErrorReportById } from "../../Services/reportService";
 import { useQuery } from "@tanstack/react-query";
 import AddTechToErrRepInput from "../../Components/AddTechToErrRepInput";
 import Textfield from "@mui/material/TextField"
-import { Button, Card, Checkbox, TextareaAutosize } from "@mui/material";
+import { Box, Button, Card, Checkbox, Paper, TextareaAutosize } from "@mui/material";
 
 const UpdateErrorReport = () => {
   const [comments, setComments] = useState([]);
-  const [ErrorReport, setErrorReport] = useState(null);
   const [rows, setRows] = useState(null);
   const { ReportId } = useParams();
 
@@ -25,64 +24,75 @@ const UpdateErrorReport = () => {
       GetErrorReportById(ReportId).then((response) => {
         console.log("response: ", response);
         setComments([...response.comments]);
-        setRows([...response.rows])
+        setRows([...response.rows]);
         return response;
+        
       }),
   });
-
 
   if (isLoading) return <div>Loading...</div>;
 
   if (error) return <div>An error has occured</div>;
 
   return (
-    <>
-      <form>
-        <div className="form-group row">
-          <label className="col-sm-2 col-form-label">Status</label>
-          <div className="col-sm-10">
-            <select>
-              <option value={false}>Out Of Order</option>
-              <option value={true}>Functioning</option>
-            </select>
-          </div>
-        </div>
-        <div className="form-group row">
-          <label className="col-sm-2 col-form-label">Assigned Technician</label>
-          <div className="col-sm-10">
+    <Box p={5} justifyContent='space-around' alignContent='center' sx={{display : 'flex'}}>
+        <Box p={1} m={1} sx={{flexDirection : 'column' }}>
+          <form>
+            <div>
+              <label>Status</label>
+              <div>
+                <select>
+                  <option value={false}>Out Of Order</option>
+                  <option value={true}>Functioning</option>
+                </select>
+              </div>
+            </div>
+
+        {
+        /* <div>
+          <label>Assigned Technician</label>
+          <div>
             <AddTechToErrRepInput ErrorReport={report} Technicans={technicians} />
           </div>
-        </div>
+        </div> */
+        }
 
-        <br></br>
+        <br/>
+        
         <button type="submit">Save</button>
+
       </form>
 
       <h2>rows: </h2>
 
-      
-      {
+      <PostRow ReportId={ReportId}/>
+
+      { 
         rows?.map((row) => (
          <PartTaskItem row={row} key={row.id}/>
         ))
       }
       
-      <br/>
 
+      </Box>
 
+      <Box p={1} m={1} sx={{ flexDirection : 'column'}}>
 
       <PostComment reportId={ReportId}/>
       
       <h2>Comments: </h2>
       
-      {comments?.map((comment) => (
-        <div>
-          <CommentItem {...comment} key={comment.id}/>
-          <br/>
-        </div>
-      ))}
-      
-    </>
+        {
+          comments?.map((comment) => (
+
+            <CommentItem {...comment} key={comment.id}/>
+
+          ))
+        }
+
+      </Box>
+
+    </Box>
   );
 };
 
@@ -107,17 +117,16 @@ const PartTaskItem = ({row}) => {
         "Content-Type": "application/json"
       },
       body: JSON.stringify(dataToSend)
-      
     }
 
     fetch(`https://grupp5elevatorapidev.azurewebsites.net/api/errorreportrow/${row.id}`, requestOptionsPUT)
-    .then(response => 
+    .then(
+      (response) => 
       {
       console.log(response);
       setcheckBoxisDone(isDone);
       }
     )
-
   } 
 
   return (
@@ -130,13 +139,15 @@ const PartTaskItem = ({row}) => {
           <p key={row.id}>{row.reportSubject}</p>
         
     </div>
-
+    <div>
       <h2>
         Comment:
       </h2>
         <p key={row.id}>{row.reportComment}</p>
-      
+      </div>
     </div>
+
+    {row.breakdownTask && <p key={row.breakdownTask.id}>{row.breakdownTask.reason}</p>}
   
     <div>
       <label>
@@ -147,6 +158,58 @@ const PartTaskItem = ({row}) => {
   <br/>
   </>
   )
+}
+
+const PostRow = ({ReportId}) => {
+  const [comment, setComment] = useState(null);
+  const [subject, setSubject] = useState(null);
+
+  var dataToSend = {
+    breakdownTask : null,
+    reportComment : comment,
+    reportSubject : subject,
+    reportId : ReportId,
+    isDone : false
+  }
+
+  const requestOptionsPOST = {
+    method: "POST",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(dataToSend),
+  };
+
+  const OnHandleSubmit = (e) => {
+    e.preventDefault();
+
+    fetch(`https://grupp5elevatorapidev.azurewebsites.net/api/errorreportrow`, requestOptions)
+    .then((response) => 
+    {
+      console.log(response);
+    })
+  } 
+
+  return (
+    <Box>
+    <form onSubmit={OnHandleSubmit}>
+      <div>
+        <div>
+          <Textfield variant="outlined" label="Subject" onChange={(e) => setSubject(e.target.value)}/>
+        </div>
+    <br/>
+        <div>
+          <TextareaAutosize style={{width : 230, height : 100}} placeholder="Comment" onChange={(e) => setComment(e.target.value)}/>
+        </div>
+
+        <Button variant="outlined" type="submit">Add row</Button>
+
+      </div>
+    </form>
+    </Box>
+  )
+
 }
 
 
@@ -161,7 +224,7 @@ const PostComment = ({reportId}) => {
     commentSubject: subject,
     commentText: comment,
     errorReportId: reportId,
-    employeeId : 'c14e56f7-4b72-4953-9e3d-8571f2075176'
+    employeeId : 'c14e56f7-4b72-4953-9e3d-8571f2075176' //Borde ändras när inloggningen har lagts till.
   };
 
   const requestOptions = {
@@ -185,7 +248,8 @@ const PostComment = ({reportId}) => {
   };
 
   return (
-    <form onSubmit={HandleSubmit} >
+    <Box>
+      <form onSubmit={HandleSubmit} >
         <div className="CommentSubject">
           <div className="CommentSubjectLabel">
           </div>
@@ -200,6 +264,7 @@ const PostComment = ({reportId}) => {
         <br/>
         <Button variant="outlined" type="submit">Send Comment</Button>
       </form>
+    </Box>
   )
 }
 
@@ -208,14 +273,14 @@ const PostComment = ({reportId}) => {
 const CommentItem = ({commentSubject, commentText}) => {
 
   return (
-    <Card variant="outlined">
-          <div className="CommentSectionSubject" >
-            <h2>{commentSubject}</h2>
-          </div>
-          <p className="CommentSectionText" >
-            {commentText}
-          </p>
-    </Card>
+    <Paper sx={{padding : '10px 20px'}}>
+          <Box>
+            <h3 sx={{margin : 0, textAlign : 'left'}}>{commentSubject}</h3>
+          </Box>
+          <Box>
+            <p sx={{textAlign : 'left'}}>{commentText}</p>
+          </Box>
+    </Paper>
   )
 }
 
